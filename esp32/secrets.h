4fd54c8d1025c1c27480b5bf0c5622338a4a58dc
Jack <jackagar2013@gmail.com>
2024-11-24 10:14:31 +1100

    Add files via upload

---
+++ esp32/secrets.h
@@ -0,0 +1,13 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define HTTP_USERNAME "" // not used
+#define HTTP_PASSWORD "" // not used
+
+#define WIFI_SSID "JMB5" // PRIVATE, WIFI NAME
+#define WIFI_PASS "5gemcourt" // PRIVATE, WIFI PASSWORD
+#define SERVER "IP NUMBERS HERE" // PRIVATE, SERVER IP ADRERSS
+#define CHAT_NAME "J"
+// #define SECURE // not used
+
+#endif // SECRETS_H {CUSTOM}
